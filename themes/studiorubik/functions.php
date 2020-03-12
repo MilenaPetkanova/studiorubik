f6e8691b505f77f98d7fec0cbc19080319f000b0
Georgi Karadzhov <jorkufrbg@gmail.com>
2020-03-12 21:19:17 +0200

    removed mediaelements.js

--- themes/studiorubik/functions.php
+++ themes/studiorubik/functions.php
@@ -12,13 +12,6 @@ function studiorubik_menus(){
     ));
 }
 
-// // Mediaelement.js Settins
-// add_action( 'wp_enqueue_scripts', 'my_mediaelement_settings' );
-// function my_mediaelement_settings() {
-//     wp_deregister_script( 'wp-mediaelement' );
-//     wp_register_script( 'wp-mediaelement', get_stylesheet_directory_uri() . "/vendors/mediaelement/wp-mediaelement.js", array( 'mediaelement' ), false, true );
-// }
-
 // Add Stylesheets and Javascript files
 function studiorubik_scripts() {
 
