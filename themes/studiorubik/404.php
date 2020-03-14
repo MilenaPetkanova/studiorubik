7e2d6782adaeaaae488b7efe198fce305b480816
Georgi Karadzhov <jorkufrbg@gmail.com>
2020-03-14 08:05:41 +0200

    Create 404.php

---
+++ themes/studiorubik/404.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<div class="error-404 not-found">
+				<header class="page-header">
+					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentynineteen' ); ?></h1>
+				</header><!-- .page-header -->
+                <a href="     https://codepen.io/Ragnarokr/pen/wvamYWL"> to be 404</a>
+			</div><!-- .error-404 -->
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
