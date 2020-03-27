6a906d00ad8de041a86853d195f3a267ca89577f
Georgi Karadzhov <jorkufrbg@gmail.com>
2020-03-27 14:14:59 +0200

    Cleaned up SCSS Files

--- themes/studiorubik/src/components/global/main.js
+++ themes/studiorubik/src/components/global/main.js
@@ -1,6 +1,6 @@
-// import './../global/variables.scss';
-// import './../global/mixins.scss';
-// import './../global/buttons.scss';
+// import './../global/_variables.scss';
+// import './../global/_mixins.scss';
+// import './../global/_buttons.scss';
 // import './../front-page/testimonials/_testimonials.scss';
 import './main.scss';
 // import './../footer/footer-menu/_footer-menu.scss';
